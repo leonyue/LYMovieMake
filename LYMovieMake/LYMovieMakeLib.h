b83c63df4bd39b2c71e45449fb50b8d4b0855e97
dj.yue <dj.yue@yuneec.com>
2017-10-25 11:36:38 +0800

    fxxx

---
+++ LYMovieMake/LYMovieMakeLib.h
@@ -0,0 +1,28 @@
+//
+//  LYMovieMakeLib.h
+//  LYMovieMake
+//
+//  Created by dj.yue on 2017/10/25.
+//  Copyright © 2017年 dj.yue. All rights reserved.
+//
+
+#ifndef LYMovieMakeLib_h
+#define LYMovieMakeLib_h
+
+#import <LYMovieMake/LYMovieMake.h>
+#import <LYMovieMake/LYMovieSlice.h>
+#import <LYMovieMake/LYMovieSound.h>
+#import <LYMovieMake/LYMovieConstants.h>
+
+///<transition and filter
+#import <LYMovieMake/LYMovieFilterStack.h>
+#import <LYMovieMake/LYMovieTransitionFilterStack.h>
+#import <LYMovieMake/LYMovieFilter.h>
+
+///<overlay
+#import <LYMovieMake/LYMovieOverlay.h>
+#import <LYMovieMake/LYMovieGIF.h>
+#import <LYMovieMake/LYMovieWaterPrint.h>
+#import <LYMovieMake/LYMovieSubTitle.h>
+
+#endif /* LYMovieMakeLib_h */
